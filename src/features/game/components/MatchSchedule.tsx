7abfaaf1fbb94015d2ef440e0c5325825a446708
닮 <dhdpekfa1@daum.net>
2024-12-03 01:31:07 +0900

    feat: 정규리그 경기 일정 부분 캘린더 퍼블리싱(#3)

--- src/features/game/components/MatchSchedule.tsx
+++ src/features/game/components/MatchSchedule.tsx
@@ -1,13 +1,14 @@
 import channelsData from '@/assets/data/broadcastChannels.json';
 import MatchInfoCarousel from '@/components/ui/carousel/MatchInfoCarousel';
+import MatchCalendar from './MatchCalendar';
 
 const MatchSchedule = () => {
   return (
     <div className="flex flex-col items-center justify-center">
+      {/* 경기 정보 */}
       <MatchInfoCarousel />
-
       {/* 달력 */}
-
+      <MatchCalendar />
       {/* 중계 채널 정보 */}
       <div className="w-full flex-col gap-2 hidden lg:flex">
         {channelsData.channels.map((channel) => (
@@ -17,8 +18,8 @@ const MatchSchedule = () => {
           >
             <strong className="flex items-centers justify-center flex-shrink-0 mr-3 px-3 w-14 bg-slate-600 text-white rounded">
               {channel.category}
-            </strong>{' '}
-            <span className="text-sm font-light whitespace-nowrap justify-start">
+            </strong>
+            <span className="text-sm font-light justify-start">
               {channel.items.map((item, index) => (
                 <span key={item.code}>
                   {item.code}
