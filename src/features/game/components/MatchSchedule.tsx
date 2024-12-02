f39389fd96dac0ea1b238755cd73b20ae56a7974
닮 <dhdpekfa1@daum.net>
2024-12-02 22:11:46 +0900

    feat: 정규리그 경기 일정 부분 캐러셀 퍼블리싱(#3)

---
+++ src/features/game/components/MatchSchedule.tsx
@@ -0,0 +1,11 @@
+import MatchInfoCarousel from '@/components/ui/carousel/MatchInfoCarousel';
+
+const MatchSchedule = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <MatchInfoCarousel />
+    </div>
+  );
+};
+
+export default MatchSchedule;
