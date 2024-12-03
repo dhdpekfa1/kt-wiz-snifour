b8e8b20cd7504ec83580383f11bffdcf38a05c80
Mina Lee <mal0070@naver.com>
2024-12-03 15:31:24 +0900

    feat: wiz park 구단 소개 페이지 레이아웃(#5)

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router';
 import Footer from './features/common/Footer';
 import Header from './features/common/Header';
 import HomePage from './pages/HomePage';
+import ParkIntro from './pages/wizPark/ParkIntro';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/wizpark/intro" element={<ParkIntro />} />
         </Routes>
         <Footer />
       </div>
