84e1dffe34c7f7b3aec2be2a264332f7ea3db33b
Miso Kim <kimms9725@naver.com>
2024-12-03 18:15:34 +0900

    feat: 메인 페이지 wiz gallery 컴포넌트(#9)

---
+++ src/features/home/components/WizGallery.tsx
@@ -0,0 +1,48 @@
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from '@/components/ui';
+
+function WizGallery() {
+  return (
+    <Card className="w-full border-none shadow-none flex flex-col items-center pb-8">
+      <CardHeader className="w-full">
+        <CardTitle>wiz Gallery</CardTitle>
+      </CardHeader>
+      <CardContent className="w-full flex flex-col items-center gap-4">
+        <Carousel className="w-full">
+          <CarouselContent>
+            <CarouselItem className="basis-1/3">
+              <div className="h-96 bg-gray-200">images</div>
+            </CarouselItem>
+            <CarouselItem className="basis-1/3">
+              <div className="h-96 bg-gray-200">images</div>
+            </CarouselItem>
+            <CarouselItem className="basis-1/3">
+              <div className="h-96 bg-gray-200">images</div>
+            </CarouselItem>
+            <CarouselItem className="basis-1/3">
+              <div className="h-96 bg-gray-200">images</div>
+            </CarouselItem>
+            <CarouselItem className="basis-1/3">
+              <div className="h-96 bg-gray-200">images</div>
+            </CarouselItem>
+          </CarouselContent>
+        </Carousel>
+      </CardContent>
+      <CardFooter>
+        <button type="button" className="border-2 px-4 py-2 rounded">
+          더 많은 사진보기
+        </button>
+      </CardFooter>
+    </Card>
+  );
+}
+
+export { WizGallery };
