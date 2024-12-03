158def04f4924144c501787cbf9841b97cefd180
Mina Lee <mal0070@naver.com>
2024-12-03 20:46:38 +0900

    feat: wizPark탭 찾아오기 페이지 레이아웃 및 카카오맵 지도 API 연결

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from 'react-router';
 import Footer from './features/common/Footer';
 import Header from './features/common/Header';
 import HomePage from './pages/HomePage';
-import ParkIntro from './pages/wizPark/ParkIntro';
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/wizpark/intro" element={<ParkIntro />} />
         </Routes>
         <Footer />
       </div>
