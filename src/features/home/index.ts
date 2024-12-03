6e5a017d4a994d8d484eaa08963e53dbd301a4c5
Miso Kim <kimms9725@naver.com>
2024-12-03 18:16:16 +0900

    feat: 메인 페이지 컴포넌트 구성 및 배치(#9)

---
+++ src/features/home/index.ts
@@ -0,0 +1,6 @@
+/** components */
+export { MainImageSlider } from './components/MainImageSlider';
+export { TeamRanking } from './components/TeamRanking';
+export { MatchInfo } from './components/MatchInfo';
+export { WizVideo } from './components/WizVideo';
+export { WizGallery } from './components/WizGallery';
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,28 @@
+import {
+  MainImageSlider,
+  MatchInfo,
+  TeamRanking,
+  WizGallery,
+  WizVideo,
+} from '@/features/home';
+
 function HomePage() {
-  return <div>Homepage</div>;
+  return (
+    <div className="flex flex-col items-center gap-4">
+      {/* 슬라이더 영역 */}
+      <MainImageSlider />
+
+      {/* 우리 팀 순위 */}
+      <TeamRanking />
+
+      {/* 경기 일정, 비디오, 갤러리 */}
+      <div className="w-[1200px] flex flex-col items-center gap-4">
+        <MatchInfo />
+        <WizVideo />
+        <WizGallery />
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
