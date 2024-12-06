fdae5155d8a3803dddba4dd306670264d3db6203
BORA <a52447879@gmail.com>
2024-12-06 22:08:50 +0900

    feat: app 컴포넌트에 스타일 수정하고 주석 달아놨어요

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,24 @@ import Header from './features/common/Header';
 import BoxScoreTab from './features/game/components/BoxScoreTab';
 import MatchScheduleTab from './features/game/components/MatchScheduleTab';
 import HomePage from './pages/HomePage';
+import NotFoundPage from './pages/NotFound';
+import PlayerPage from './pages/media/FirstPitch';
+import HighlightPage from './pages/media/Highlight';
+import NewsPage from './pages/media/News';
+import PhotoPage from './pages/media/Photo';
+import StoryPage from './pages/media/Story';
 import CheerSong from './pages/player/CheerSong';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="w-screen bg-wiz-black flex flex-col items-center">
+      <div className="w-full bg-wiz-black flex flex-col items-center">
         <Header />
-        <div className="w-[1200px]">
+        {/*
+          데스크탑 -> 1240px (padding 포함, 실제컨텐츠 1200px),
+          모바일 -> 100%
+        */}
+        <div className="w-full px-5 lg:max-w-[1240px]">
           <Routes>
             {/* 메인 */}
             <Route path="/" element={<HomePage />} />
@@ -25,6 +35,16 @@ function App() {
 
             {/* Player */}
             <Route path="/player/song" element={<CheerSong />} />
+
+            {/* Media */}
+            <Route path="/media/wiznews" element={<NewsPage />} />
+            <Route path="/media/wizpress" element={<NewsPage />} />
+            <Route path="/media/wizstory" element={<StoryPage />} />
+            <Route path="/media/photos/:id" element={<PhotoPage />} />
+            <Route path="/media/firstpitch" element={<PlayerPage />} />
+            <Route path="/media/highlight" element={<HighlightPage />} />
+            {/* 404 */}
+            <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </div>
         <Footer />
