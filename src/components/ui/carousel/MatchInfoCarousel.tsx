1220a7b6db997d7ad6c4bd82031c36712136c36c
닮 <dhdpekfa1@daum.net>
2024-12-10 18:14:22 +0900

    refactor: 컴포넌트 분리(#49)

--- src/components/ui/carousel/MatchInfoCarousel.tsx
+++ src/components/ui/carousel/MatchInfoCarousel.tsx
@@ -1,14 +1,12 @@
-import { Card, CardContent } from '@/components/ui';
-import TeamInfo from '@/features/common/TeamInfo';
 import { getMonthSchedule } from '@/features/game/apis/matchSchedule';
-import { GameSchedule } from '@/features/game/components/calender/MatchCalendar';
+import { CarouselCard } from '@/features/game/components';
+import { GameSchedule } from '@/features/game/types';
 import { useMatchStore } from '@/store/useMatchStore';
-import { format, parse } from 'date-fns';
+import { format, isValid, parse } from 'date-fns';
 import { useCallback, useEffect, useRef, useState } from 'react';
 import {
   Carousel,
   CarouselContent,
-  CarouselItem,
   CarouselNext,
   CarouselPrevious,
 } from './carousel';
@@ -40,9 +38,9 @@ const MatchInfoCarousel = () => {
 
         // 이전 달로 이동
         const currentDate = new Date(
-          parseInt(yearMonth.slice(0, 4)), // 연도
-          parseInt(yearMonth.slice(4, 6)) - 1, // 월
-          1 // 첫 번째 날
+          parseInt(yearMonth.slice(0, 4)),
+          parseInt(yearMonth.slice(4, 6)) - 1,
+          1
         );
         currentDate.setMonth(currentDate.getMonth() - 1);
         yearMonth = format(currentDate, 'yyyyMM');
@@ -74,102 +72,25 @@ const MatchInfoCarousel = () => {
     }
   }, [selectedDate, matchData, formatDate]);
 
-  // 최근 경기 이후 로직
   const today = new Date();
-  const hasUpcomingGames = matchData.some(
-    (game) => parse(game.gameDate.toString(), 'yyyyMMdd', new Date()) >= today
-  );
+  const hasUpcomingGames = matchData.some((game) => {
+    const gameDateString = game.gameDate?.toString(); // gameDate를 문자열로 변환
+    if (!gameDateString) return false;
+    const parsedDate = parse(gameDateString, 'yyyyMMdd', new Date());
+    return isValid(parsedDate) && parsedDate >= today; // 유효한 날짜인지 확인 후 비교
+  });
 
   return (
     <div className="w-full max-w-2xl min-w-full overflow:hidden">
       <Carousel ref={carouselRef} className="relative max-w-full">
-        <CarouselContent className="-ml-2">
-          {matchData.map((data, index) => (
-            <CarouselItem
-              key={data.gameDate || index}
-              className={
-                'pl-1 md:basis-1/2 lg:basis-1/3 transition-transform duration-300 w-fit'
-              }
-            >
-              <div className="p-1">
-                <Card className="min-w-80 w-full rounded border-[#35383e] shadow-[#5b5f65]">
-                  <CardContent className="flex flex-col gap-2 items-center justify-between p-5 bg-[#35383e]">
-                    <div className="flex flex-col h-48 items-center justify-between p-2">
-                      {/* 날짜 라벨 */}
-                      <h4
-                        className={`text-white px-6 py-1 rounded-full ${
-                          data.outcome === '패' ? 'bg-wiz-red' : 'bg-wiz-black'
-                        }`}
-                      >
-                        {formatDate(data.displayDate)}
-                      </h4>
-
-                      <div className="flex gap-6 items-center justify-center px-6">
-                        {/* team1 */}
-                        <TeamInfo
-                          tabType="MatchScheduleTab"
-                          teamName={data.home}
-                          logoUrl={data.homeLogo || ''}
-                          player={data.homeKey}
-                          result={
-                            data.homeScore > data.visitScore ? 'win' : 'lose'
-                          }
-                        />
-
-                        {/* 스코어, 승패, 경기 정보 버튼 */}
-                        <div className="flex flex-col items-center justify-center">
-                          <h4 className="mb-4 font-normal text-3xl leading-none text-wiz-white">
-                            {data.homeScore} : {data.visitScore}
-                          </h4>
-                          <div className="flex gap-2">
-                            <p className="mb-4 text-lg leading-none text-wiz-red">
-                              {data.outcome}
-                            </p>
-                          </div>
-                          <button
-                            type="button"
-                            className="bg-gray-400 text-white rounded-full hover:bg-gray-500 py-1 px-3 w-24"
-                          >
-                            경기 정보
-                          </button>
-                        </div>
-                        {/* team2 */}
-                        <TeamInfo
-                          tabType="MatchScheduleTab"
-                          teamName={data.visit}
-                          logoUrl={data.visitLogo || ''}
-                          player={data.visitKey}
-                          result={
-                            data.homeScore > data.visitScore ? 'lose' : 'win'
-                          }
-                        />
-                      </div>
-                    </div>
-                  </CardContent>
-                </Card>
-              </div>
-            </CarouselItem>
+        <CarouselContent className="-ml-1">
+          {matchData.map((data) => (
+            <CarouselCard key={data.gameDate} data={data} />
           ))}
-
-          {!hasUpcomingGames && (
-            <CarouselItem className="pl-1 md:basis-1/2 lg:basis-1/3 transition-transform duration-300 w-fit">
-              <div className="p-1">
-                <Card className="min-w-80 w-full rounded border-[#35383e] shadow-[#5b5f65]">
-                  <CardContent className="flex flex-col gap-2 items-center justify-between p-5 bg-[#35383e]">
-                    <div className="flex flex-col gap-5 h-48 items-center p-2">
-                      <div className="top-0 w-full h-7 bg-wiz-black text-white p-1 rounded-2xl" />
-                      <p className="mb-4 text-wiz-white">
-                        예정된 경기가 없습니다.
-                      </p>
-                    </div>
-                  </CardContent>
-                </Card>
-              </div>
-            </CarouselItem>
-          )}
+          {!hasUpcomingGames && <CarouselCard data={null} />}
         </CarouselContent>
-        <CarouselPrevious className="absolute left-[-28px] top-1/2 -translate-y-1/2 z-30 bg-gray-600 text-white hover:bg-[#222] hover:text-wiz-white p-2 rounded-full" />
-        <CarouselNext className="absolute right-[-24px] top-1/2 -translate-y-1/2 z-20 bg-gray-600 text-white hover:bg-[#222] hover:text-wiz-white p-2 rounded-full" />
+        <CarouselPrevious className="absolute left-0 top-1/2 -translate-y-1/2 z-30 bg-gray-600 text-white hover:bg-[#222] hover:text-wiz-white p-2 rounded-full" />
+        <CarouselNext className="absolute right-0 top-1/2 -translate-y-1/2 z-20 bg-gray-600 text-white hover:bg-[#222] hover:text-wiz-white p-2 rounded-full" />
       </Carousel>
     </div>
   );
