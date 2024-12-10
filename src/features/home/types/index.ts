efb476444996a023a14f339bc795c0d7a82ca869
Miso Kim <kimms9725@naver.com>
2024-12-10 18:33:26 +0900

    feat: wiz photo api 연결(#46)

--- src/features/home/types/index.ts
+++ src/features/home/types/index.ts
@@ -52,3 +52,26 @@ export interface Video {
   videoLink: string;
   viewCnt: number;
 }
+
+export interface Photo {
+  artcNextSeq: number;
+  artcPrevSeq: number;
+  artcSeq: number;
+  artcSubTitle: string;
+  artcTitle: string;
+  boardCatSeq: number;
+  boardCode: string;
+  contentsDate: string;
+  delYn: string;
+  endDttm: number;
+  imgFilePath: string;
+  maxArticlePerPage: number;
+  regDttm: number;
+  regr: string;
+  startDttm: number;
+  totalPage: number;
+  updDttm: number;
+  updr: string;
+  useYn: string;
+  viewCnt: number;
+}
