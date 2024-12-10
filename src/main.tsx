37cf6ca4b71364b15cf53caf7273109da59e5aa6
BORA <a52447879@gmail.com>
2024-12-10 22:19:24 +0900

    feat: react-query 적용

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import AppProviders from './providers/AppProviders.tsx';
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <App />
+      <AppProviders>
+        <App />
+      </AppProviders>
     </StrictMode>
   );
 }
