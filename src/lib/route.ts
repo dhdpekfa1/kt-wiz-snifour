582723656b2f7e51574e58cc5894f87ab3966c9d
BORA <a52447879@gmail.com>
2024-12-11 16:19:43 +0900

    feat: router utils

--- src/lib/route.ts
+++
@@ -1,6 +0,0 @@
-import { ApiRoutes, PageRoutes } from '@/constants/route';
-import { compile } from 'path-to-regexp';
-
-// URL 생성 유틸리티 함수 (URL 경로에 포함되는 동적 파라미터 처리)
-export const toUrl = (path: PageRoutes | ApiRoutes, params?: object) =>
-  compile(path, { encode: encodeURIComponent })(params);
