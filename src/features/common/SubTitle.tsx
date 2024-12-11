e73234903957a7d4505baaed2a8782605f59e8f5
Miso Kim <kimms9725@naver.com>
2024-12-11 17:07:51 +0900

    style: subtitle에 classname 받을수 있도록 변경(#55)

--- src/features/common/SubTitle.tsx
+++ src/features/common/SubTitle.tsx
@@ -1,8 +1,16 @@
-const SubTitle = ({ title }: { title: string }) => {
+import { cn } from '@/lib/utils';
+
+const SubTitle = ({
+  title,
+  className,
+}: {
+  title: string;
+  className?: string;
+}) => {
   return (
-    <div className="flex items-center gap-2">
+    <div className={cn('flex items-center gap-2 text-white', className || '')}>
       <div className="h-4 w-1 bg-[#ec0a0b]" />
-      <h4 className="font-semibold text-white text-2xl">{title}</h4>
+      <h4 className="font-semibold text-2xl">{title}</h4>
     </div>
   );
 };
