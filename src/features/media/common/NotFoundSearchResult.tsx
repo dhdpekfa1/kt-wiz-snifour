76cd9b6030dc34a99c7e62545147aa6329578298
BORA <a52447879@gmail.com>
2024-12-11 17:21:11 +0900

    feat: 검색어가 없을때 처리 (#45)

---
+++ src/features/media/common/NotFoundSearchResult.tsx
@@ -0,0 +1,9 @@
+const NotFoundResult = () => {
+  return (
+    <div className="flex items-center justify-center min-h-[400px]">
+      검색어에 맞는 결과가 없어요. 다시 한 번 검색어를 확인해주세요.
+    </div>
+  );
+};
+
+export default NotFoundResult;
