3cc4d5dd0512f5754ff12a06e65faabaa082afa6
BORA <a52447879@gmail.com>
2024-12-11 17:19:12 +0900

    design: fix sub layout padding

--- src/features/common/Layout.tsx
+++ src/features/common/Layout.tsx
@@ -9,9 +9,7 @@ const Layout = ({ header, children }: LayoutProps) => {
   return (
     <div className={cn('w-full bg-wiz-black relative mb-8')}>
       <section className={cn('w-full')}>{header}</section>
-      <section className={cn('w-full pb-14 text-wiz-white', 'md:pb-24')}>
-        {children}
-      </section>
+      <section className={cn('w-full text-wiz-white')}>{children}</section>
     </div>
   );
 };
