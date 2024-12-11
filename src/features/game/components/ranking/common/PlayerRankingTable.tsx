ba3b85f5de7328008e5a8833dac9bdd6e96c474a
Miso Kim <kimms9725@naver.com>
2024-12-12 00:41:01 +0900

    feat: 투수 순위 탭 레이아웃(#55)

---
+++ src/features/game/components/ranking/common/PlayerRankingTable.tsx
@@ -0,0 +1,92 @@
+import { useState } from 'react';
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  getSortedRowModel,
+  SortingState,
+  useReactTable,
+} from '@tanstack/react-table';
+
+import { OverallPitcherRank } from '@/features/common/types/Pitchers';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui';
+import { cn } from '@/lib/utils';
+
+interface PlayerRankingTableProps {
+  data: OverallPitcherRank[];
+  columns: ColumnDef<OverallPitcherRank>[];
+  kt?: boolean;
+}
+
+function PlayerRankingTable({ data, columns, kt }: PlayerRankingTableProps) {
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    state: {
+      sorting,
+    },
+  });
+
+  return (
+    <Table className="rounded-xl overflow-hidden">
+      <TableHeader>
+        {table.getHeaderGroups().map((headerGroup) => (
+          <TableRow key={headerGroup.id} className="border-none">
+            {headerGroup.headers.map((header) => (
+              <TableHead key={header.id} className="text-center px-2">
+                {header.isPlaceholder
+                  ? null
+                  : flexRender(
+                      header.column.columnDef.header,
+                      header.getContext()
+                    )}
+              </TableHead>
+            ))}
+          </TableRow>
+        ))}
+      </TableHeader>
+      <TableBody>
+        {table.getRowModel().rows?.length ? (
+          table.getRowModel().rows.map((row, index) => (
+            <TableRow
+              key={row.id}
+              data-state={row.getIsSelected() && 'selected'}
+              className={cn(
+                'text-center',
+                kt && index < 3
+                  ? 'bg-wiz-red bg-opacity-80 font-bold border-b-red-800'
+                  : 'bg-wiz-white text-black',
+                !kt &&
+                  row.original.teamName === 'KT' &&
+                  'bg-wiz-red bg-opacity-80 font-bold text-white'
+              )}
+            >
+              {row.getVisibleCells().map((cell) => (
+                <TableCell key={cell.id} className="px-2">
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))
+        ) : (
+          <TableRow>
+            <TableCell colSpan={columns.length}>결과가 없습니다.</TableCell>
+          </TableRow>
+        )}
+      </TableBody>
+    </Table>
+  );
+}
+
+export { PlayerRankingTable };
---
+++ src/features/game/components/ranking/pitcher/AllPitcherRankingTab.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import { OverallPitcherRank } from '@/features/common/types/Pitchers';
+import { API_URL } from '@/constants/api-url';
+import { PlayerRankingTable } from '@/features/game/components/ranking';
+import { pitcherColumns } from '@/constants/player-rank-colums';
+
+function AllPitcherRankingTab() {
+  const [ranking, setRanking] = useState<OverallPitcherRank[]>([]);
+
+  useEffect(() => {
+    const getPitcherRanking = async () => {
+      try {
+        const { data, status } = await axios.get(
+          `${API_URL}/game/rank/total/pitcher?gyear=2024&pname=&sortKey=ERA`
+        );
+
+        if (status === 200 && data) {
+          setRanking(data.data.list || []);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    getPitcherRanking();
+  }, []);
+
+  return <PlayerRankingTable data={ranking} columns={pitcherColumns} />;
+}
+
+export { AllPitcherRankingTab };
