1605dab5eb5b53952b347bd49aa9925546e98e17
BORA <a52447879@gmail.com>
2024-12-12 16:33:32 +0900

    remove: api 파일 폴더 이동 (#45)

---
+++ src/features/media/apis/news/NewsApi.query.ts
@@ -0,0 +1,79 @@
+import { AxiosError } from 'axios';
+import { newsApi } from './NewsApi';
+import { useQuery } from '@tanstack/react-query';
+import { keepPreviousData } from '@tanstack/react-query';
+import { createListViewItem } from '@/features/media/services';
+import { Parameter, isNotNullish, UseQueryParams } from '@/lib';
+import {
+  ListDataType,
+  NewsResponse,
+  NewsDetailResponse,
+  ListViewType,
+} from '@/features/media/types';
+
+// 쿼리 키 정의
+export const NEWS_API_QUERY_KEY = {
+  /** 뉴스 목록 조회 쿼리 키 생성 */
+  GET_LIST: (params?: Parameter<typeof newsApi.getNewsList>) =>
+    ['news-list', params].filter(isNotNullish),
+
+  /** 특정 뉴스 상세 정보 조회 쿼리 키 생성 */
+  GET_BY_SEQ: (artcSeq: Parameter<typeof newsApi.getNewsBySeq>) =>
+    ['news-by-seq', artcSeq].filter(isNotNullish),
+};
+
+/**
+ * [GET] /api/article/newslist?searchWord=
+ * @param params 뉴스 목록 조회에 필요한 파라미터
+ * @returns 리스트 타입으로 변환된 뉴스 목록 데이터와 총 데이터 개수
+ */
+export function useGetNewsList(
+  params?: UseQueryParams<
+    typeof newsApi.getNewsList,
+    AxiosError,
+    NewsResponse, // 실제 응답
+    ListDataType // 변환된 응답
+  >
+) {
+  return useQuery({
+    queryKey: NEWS_API_QUERY_KEY.GET_LIST(params?.variables),
+    queryFn: async () => {
+      const response = await newsApi.getNewsList(params?.variables);
+      return response;
+    },
+    select: (res: NewsResponse): ListDataType => ({
+      list: res.data.list
+        .filter((item) => item.useYn === 'Y')
+        .map(createListViewItem),
+      searchCount: res.data.searchCount,
+    }),
+    gcTime: 10 * 60 * 1000, // 캐시타임
+    refetchOnMount: false, // 컴포넌트 마운트 시 쿼리 재요청 여부
+    refetchOnWindowFocus: false, // 창 포커스 시 쿼리 재요청 여부
+    placeholderData: keepPreviousData, // 페이지 네이션이 있을 경우에는 추가해주세요. 이전 데이터를 유지해줍니다.
+    ...params?.options,
+  });
+}
+
+export function useGetNewsBySeq(
+  params: UseQueryParams<
+    typeof newsApi.getNewsBySeq,
+    AxiosError,
+    NewsDetailResponse, // 실제 응답
+    ListViewType // 변환된 응답
+  >
+) {
+  const queryKey = NEWS_API_QUERY_KEY.GET_BY_SEQ(params.variables);
+  return useQuery({
+    queryKey,
+    queryFn: async () => {
+      const response = await newsApi.getNewsBySeq(params.variables);
+      return response;
+    },
+    select: (res: NewsDetailResponse) => createListViewItem(res.data.article),
+    gcTime: 10 * 60 * 1000, // 캐시타임
+    refetchOnMount: false, // 컴포넌트 마운트 시 쿼리 재요청 여부
+    refetchOnWindowFocus: false, // 창 포커스 시 쿼리 재요청 여부
+    ...params.options,
+  });
+}
