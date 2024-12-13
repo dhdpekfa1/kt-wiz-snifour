94da5d5ac4153b2321a4a98165d4738efe710458
Miso Kim <kimms9725@naver.com>
2024-12-13 16:06:57 +0900

    refactor: 커스텀 툴팁 수정(#61)

--- src/features/game/components/ranking/common/CustomTooltip.tsx
+++ src/features/game/components/ranking/common/CustomTooltip.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import { useMemo } from 'react';
 import { TooltipProps } from 'recharts';
 import CustomIndicator from './CustomIndicator';
 
@@ -17,46 +17,43 @@ interface CustomTooltipProps extends TooltipProps<string, string> {
   type: string;
 }
 
-const CustomTooltip = React.forwardRef<HTMLDivElement, CustomTooltipProps>(
-  ({ active, payload, type }) => {
-    if (active && payload && payload.length) {
-      const { playerName, era, wra, gamenum, teamName, hra, ops, crowd } =
-        payload[0].payload as Payload;
-      console.log(payload);
+function CustomTooltip({ active, payload, type }: CustomTooltipProps) {
+  if (active && payload && payload.length) {
+    const { playerName, era, wra, gamenum, teamName, hra, ops, crowd } =
+      payload[0].payload as Payload;
 
-      const label = useMemo(() => {
-        if (type === 'pitcher' || type === 'batter') {
-          return `${playerName}(${teamName})`;
-        }
-        return teamName;
-      }, [type, playerName, teamName]);
+    const label = useMemo(() => {
+      if (type === 'pitcher' || type === 'batter') {
+        return `${playerName}(${teamName})`;
+      }
+      return teamName;
+    }, [type, playerName, teamName]);
 
-      return (
-        <div className="w-fit bg-white text-black p-2 border rounded shadow-md">
-          <div className="flex items-center justify-between mb-2 gap-8">
-            <p className="font-bold ">{label}</p>
-            {gamenum && <p className="text-sm">{gamenum}번 출장</p>}
-          </div>
-          {type === 'pitcher' && (
-            <>
-              <CustomIndicator indicator="ERA" value={era} />
-              <CustomIndicator indicator="승률" value={wra} />
-            </>
-          )}
-          {type === 'batter' && (
-            <>
-              <CustomIndicator indicator="타율" value={hra} />
-              <CustomIndicator indicator="OPS" value={ops} />
-            </>
-          )}
-          {type === 'crowd' && (
-            <CustomIndicator indicator="관중" value={crowd.toLocaleString()} />
-          )}
+    return (
+      <div className="w-fit bg-white text-black p-2 border rounded shadow-md">
+        <div className="flex items-center justify-between mb-2 gap-8">
+          <p className="font-bold ">{label}</p>
+          {gamenum && <p className="text-sm">{gamenum}번 출장</p>}
         </div>
-      );
-    }
-    return null;
+        {type === 'pitcher' && (
+          <>
+            <CustomIndicator indicator="ERA" value={era} />
+            <CustomIndicator indicator="승률" value={wra} />
+          </>
+        )}
+        {type === 'batter' && (
+          <>
+            <CustomIndicator indicator="타율" value={hra} />
+            <CustomIndicator indicator="OPS" value={ops} />
+          </>
+        )}
+        {type === 'crowd' && (
+          <CustomIndicator indicator="관중" value={crowd.toLocaleString()} />
+        )}
+      </div>
+    );
   }
-);
+  return null;
+}
 
 export default CustomTooltip;
