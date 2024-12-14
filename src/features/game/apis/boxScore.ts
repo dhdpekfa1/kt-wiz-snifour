5e375d33eb7207309fe59148d93539f123b83f60
Mina Lee <mal0070@naver.com>
2024-12-14 14:28:47 +0900

    fix: 박스스코어 api 호출 식 오타 수정(#40)

--- src/features/game/apis/boxScore.ts
+++ src/features/game/apis/boxScore.ts
@@ -1,17 +1,13 @@
 import axios from 'axios';
-
 const API_URL = import.meta.env.VITE_REACT_APP_API_URL;
-
-export const getMatchData = async (gameDate: string, gmKey: string) => {
+export const getMatchData = async (gameDate: string, gmkey: string) => {
   try {
-    const res = await axios.get(
-      `${API_URL}/game/boxscore/${gameDate}/${gmKey}`
-    );
-
+    const res = await axios.get(`${API_URL}/game/boxscore`, {
+      params: { gameDate, gmkey },
+    });
     if (res.status !== 200) {
       throw new Error(`Failed to fetch data. Status code: ${res.status}`);
     }
-
     return res.data.data.list;
   } catch (err) {
     console.error('박스스코어 api GET 에러: ', err);
