79607bc19622d31f0fa9b897b631a10f4eb8f736
Miso Kim <kimms9725@naver.com>
2024-12-17 17:06:33 +0900

    feat: 투수 페이지 헤더 이미지 추가(#73)

--- src/pages/player/PitcherPage.tsx
+++ src/pages/player/PitcherPage.tsx
@@ -1,13 +1,26 @@
+import Banner from '@/features/common/Banner';
 import Breadcrumb from '@/features/common/Breadcrumb';
 import { PlayerList } from '@/features/player/components';
 
 function PitcherPage() {
   return (
-    <div className="my-20">
-      <Breadcrumb />
+    <div>
+      <Banner>
+        <Banner.Image
+          src="https://placehold.co/1200x200/141414/642521?text=WIZ+PITCHERS"
+          alt="KT WIZ 투수"
+        />
+        <Banner.Overlay>
+          <Banner.Heading title="투수" />
+          <Banner.Description description="KT Wiz의 자랑스런 '첫 번째 선수단'을 소개합니다." />
+        </Banner.Overlay>
+      </Banner>
 
-      {/* 투수 리스트 */}
-      <PlayerList playerList={[]} endpoint="pitcher" />
+      <div className="my-20">
+        <Breadcrumb />
+        {/* 투수 리스트 */}
+        <PlayerList playerList={[]} endpoint="pitcher" />
+      </div>
     </div>
   );
 }
