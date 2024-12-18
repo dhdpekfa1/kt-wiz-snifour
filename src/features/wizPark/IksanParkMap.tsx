67d05ac42d05fb3794b63fc95f2d8af53c650697
닮 <dhdpekfa1@daum.net>
2024-12-18 16:43:50 +0900

    feat: 익산야구장 라우트 연결(#76)

---
+++ src/features/wizPark/IksanParkMap.tsx
@@ -0,0 +1,35 @@
+import useKakaoLoader from '@/hooks/useKakaoLoader';
+import {
+  // biome-ignore lint/suspicious/noShadowRestrictedNames: <explanation>
+  Map,
+  MapMarker,
+  MapTypeId,
+  ZoomControl,
+} from 'react-kakao-maps-sdk';
+
+function IksanParkMap() {
+  useKakaoLoader();
+  const position = { lat: 35.967842, lng: 127.0075477 };
+
+  return (
+    <>
+      <Map // 지도를 표시할 Container
+        id="map"
+        center={position}
+        style={{
+          // 지도의 크기
+          width: '100%',
+          height: '600px',
+        }}
+        level={3} // 지도의 확대 레벨
+      >
+        <MapMarker position={position} />
+        <ZoomControl position={'RIGHT'} />
+        {/* 실시간 교통 정보 표시 */}
+        <MapTypeId type={'TRAFFIC'} />
+      </Map>
+    </>
+  );
+}
+
+export default IksanParkMap;
