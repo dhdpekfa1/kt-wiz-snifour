bf06b14c9e6f68fe6860bc2f08b74ecae906469b
Miso Kim <kimms9725@naver.com>
2024-12-19 18:01:04 +0900

    feat: 커스텀 툴팁 (#73)

--- src/features/common/CustomBarChart.tsx
+++ src/features/common/CustomBarChart.tsx
@@ -1,4 +1,12 @@
-import { Bar, BarChart, CartesianGrid, Tooltip, XAxis, YAxis } from 'recharts';
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Tooltip,
+  TooltipProps,
+  XAxis,
+  YAxis,
+} from 'recharts';
 import { RecentRecord, YearRecord } from '../player/types/record';
 import { ChartContainer } from '@/components/ui/chart';
 
@@ -14,14 +22,39 @@ interface CustomBarChartProps {
   XAxisKey: string;
 }
 
+interface CustomTooltipProps extends TooltipProps<string, string> {
+  dataKey: string;
+}
+
+function CustomTooltip({ active, payload, dataKey }: CustomTooltipProps) {
+  if (active && payload && payload.length) {
+    const value = payload[0].value;
+
+    return (
+      <div className="bg-white flex items-center gap-2 rounded p-2">
+        <span className="w-fit text-sm bg-wiz-black text-white rounded text-center px-2 py-1">
+          {dataKey}
+        </span>
+        <span className="text-black">{value}</span>
+      </div>
+    );
+  }
+
+  return null;
+}
+
 function CustomBarChart({ data, config, XAxisKey }: CustomBarChartProps) {
+  const key: keyof typeof config =
+    Object.keys(config).find((key) => config[key].isActive) || '';
+  const dataKey = config[key].label;
+
   return (
     <ChartContainer config={config} className="w-full h-52 mt-4">
       <BarChart accessibilityLayer data={data}>
         <CartesianGrid vertical={false} strokeOpacity={0.1} />
         <XAxis dataKey={XAxisKey} tickLine={false} axisLine={false} />
         <YAxis tickLine={false} axisLine={false} />
-        <Tooltip />
+        <Tooltip content={<CustomTooltip dataKey={dataKey} />} />
         {Object.entries(config).map(([dataKey, value]) =>
           value.isActive ? (
             <Bar
--- src/features/common/CustomLineChart.tsx
+++ src/features/common/CustomLineChart.tsx
@@ -1,5 +1,13 @@
 import { ChartContainer } from '@/components/ui/chart';
-import { CartesianGrid, Line, LineChart, XAxis, YAxis } from 'recharts';
+import {
+  CartesianGrid,
+  Line,
+  LineChart,
+  Tooltip,
+  TooltipProps,
+  XAxis,
+  YAxis,
+} from 'recharts';
 import { RecentRecord, YearRecord } from '../player/types/record';
 
 interface CustomLineChartProps {
@@ -14,14 +22,39 @@ interface CustomLineChartProps {
   XAxisKey: string;
 }
 
+interface CustomTooltipProps extends TooltipProps<string, string> {
+  dataKey: string;
+}
+
+function CustomTooltip({ active, payload, dataKey }: CustomTooltipProps) {
+  if (active && payload && payload.length) {
+    const value = payload[0].value;
+
+    return (
+      <div className="bg-white flex items-center gap-2 rounded p-2">
+        <span className="w-fit text-sm bg-wiz-black text-white rounded text-center px-2 py-1">
+          {dataKey}
+        </span>
+        <span className="text-black">{value}</span>
+      </div>
+    );
+  }
+
+  return null;
+}
+
 function CustomLineChart({ data, config, XAxisKey }: CustomLineChartProps) {
+  const key: keyof typeof config =
+    Object.keys(config).find((key) => config[key].isActive) || '';
+  const dataKey = config[key].label;
+
   return (
     <ChartContainer config={config} className="w-full h-52 mt-4">
       <LineChart accessibilityLayer data={data}>
         <CartesianGrid vertical={false} strokeOpacity={0.1} />
         <XAxis dataKey={XAxisKey} tickLine={false} axisLine={false} />
         <YAxis tickLine={false} axisLine={false} />
-        {/* <Tooltip /> */}
+        <Tooltip content={<CustomTooltip dataKey={dataKey} />} />
         {Object.entries(config).map(([dataKey, value]) =>
           value.isActive ? (
             <Line
