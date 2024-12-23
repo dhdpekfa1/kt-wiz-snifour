7bcc5d9b5d03b5664b704d8ac33f8f4599ae1d06
닮 <dhdpekfa1@daum.net>
2024-12-23 15:13:29 +0900

    feat: 표, 차트 버튼 생성 및 해당 데이터 분기처리(#84)

--- src/features/game/components/ranking/team/TeamPitcherRankingTable.tsx
+++ src/features/game/components/ranking/team/TeamPitcherRankingTable.tsx
@@ -1,10 +1,13 @@
-import { teamPitcherRankColumns } from '@/constants/columns/team-rank-colums';
-import DataTable from '@/features/common/DataTable';
 // import { TeamPitcherRank } from '@/features/common/types/pitchers';
 // import { useTeamRank } from '@/assets/hooks/ranking/useTeamRank';
 import { teamPitcher as mockRanking } from '@/assets/data/__test__/mockRanking.json';
+import { teamPitcherRankColumns } from '@/constants/columns/team-rank-colums';
+import DataTable from '@/features/common/DataTable';
+import { useState } from 'react';
 
 function TeamPitcherRankingTable() {
+  const [selectedTab, setSelectedTab] = useState<'table' | 'chart'>('table');
+
   // const { ranking, loading, error } = useTeamRank('pitcher');
 
   // if (!ranking.length || loading) {
@@ -16,11 +19,37 @@ function TeamPitcherRankingTable() {
   // }
 
   return (
-    <DataTable
-      data={mockRanking}
-      columns={teamPitcherRankColumns}
-      domain="all"
-    />
+    <div>
+      <div className="flex items-center gap-2 justify-end">
+        <button
+          type="button"
+          className={`text-center w-12 bg-white text-black px-2 py-1 text-sm rounded ${
+            selectedTab === 'table' ? 'bg-wiz-red text-wiz-white' : 'bg-white'
+          }`}
+          onClick={() => setSelectedTab('table')}
+        >
+          표
+        </button>
+        <button
+          type="button"
+          className={`text-center w-12 bg-white text-black px-2 py-1 text-sm rounded ${
+            selectedTab === 'chart' ? 'bg-wiz-red text-wiz-white' : 'bg-white'
+          }`}
+          onClick={() => setSelectedTab('chart')}
+        >
+          차트
+        </button>
+      </div>
+      {selectedTab === 'table' ? (
+        <DataTable
+          data={mockRanking}
+          columns={teamPitcherRankColumns}
+          domain="all"
+        />
+      ) : (
+        <div>check</div>
+      )}
+    </div>
   );
 }
 
