04fc9b55db28b04f4d867b70d07ec136f1fccfc1
닮 <dhdpekfa1@daum.net>
2024-12-24 15:34:55 +0900

    refactor: 동일하게 사용되는 차트, 표 형식 타자기록, 투수기록  공통 컴포넌트 분리(#90)

--- src/features/game/components/ranking/team/TeamBatterRankingView.tsx
+++ src/features/game/components/ranking/team/TeamBatterRankingView.tsx
@@ -1,23 +1,13 @@
 import { teamBatter as mockRanking } from '@/assets/data/__test__/mockRanking.json';
-import data from '@/assets/data/__test__/season-team-rank/seasonBatter.json';
+import mockBatterData from '@/assets/data/__test__/season-team-rank/seasonBatter.json';
 import { TeamRankingBatterConfig } from '@/constants/chart-config';
 import { teamBatterRankColumns } from '@/constants/columns/team-rank-colums';
-import CustomBarChart from '@/features/common/CustomBarChart';
 // import { TeamBatterRank } from '@/features/common/types/batters';
 // import { useTeamRank } from '@/assets/hooks/ranking/useTeamRank';
-import DataTable from '@/features/common/DataTable';
-import { Config } from '@/features/player/components/PlayerRecordChart';
-import { SeasonBatter } from '@/features/player/types/record';
-import { useState } from 'react';
 
-const seasonBatterData: SeasonBatter[] = data.data.list;
+import TeamRankingView from '../common/TeamRankingView';
 
 function TeamBatterRankingView() {
-  const [selectedTab, setSelectedTab] = useState<'table' | 'chart'>('table');
-  const [chartConfig, setChartConfig] = useState<Config>(
-    TeamRankingBatterConfig
-  );
-
   // const { ranking, loading, error } = useTeamRank('batter');
 
   // if (!ranking.length || loading) {
@@ -28,85 +18,14 @@ function TeamBatterRankingView() {
   //   return <div>{error}</div>;
   // }
 
-  const handleConfig = (dataKey: keyof Config) => {
-    const currentConfig = Object.keys(chartConfig).filter(
-      (key) => chartConfig[key].isActive
-    )[0];
-
-    if (currentConfig === dataKey) {
-      return;
-    }
-
-    setChartConfig((prev) => {
-      // 모든 항목을 비활성화한 뒤 클릭한 버튼만 활성화
-      const newConfig = Object.keys(prev).reduce((acc, key) => {
-        acc[key] = { ...prev[key], isActive: false }; // 모든 항목 비활성화
-        return acc;
-      }, {} as Config);
-
-      // 클릭한 버튼만 활성화
-      newConfig[dataKey] = {
-        ...prev[dataKey],
-        isActive: !prev[dataKey].isActive,
-      };
-      return newConfig;
-    });
-  };
-
   return (
-    <div>
-      <div className="flex items-center gap-2 justify-end">
-        <button
-          type="button"
-          className={`text-center w-12 bg-white text-black px-2 py-1 text-sm rounded ${
-            selectedTab === 'table' ? 'bg-wiz-red text-wiz-white' : 'bg-white'
-          }`}
-          onClick={() => setSelectedTab('table')}
-        >
-          표
-        </button>
-        <button
-          type="button"
-          className={`text-center w-12 bg-white text-black px-2 py-1 text-sm rounded ${
-            selectedTab === 'chart' ? 'bg-wiz-red text-wiz-white' : 'bg-white'
-          }`}
-          onClick={() => setSelectedTab('chart')}
-        >
-          차트
-        </button>
-      </div>
-      {selectedTab === 'table' ? (
-        <DataTable
-          data={mockRanking}
-          columns={teamBatterRankColumns}
-          domain="all"
-        />
-      ) : (
-        <div>
-          <CustomBarChart
-            data={seasonBatterData}
-            config={chartConfig}
-            XAxisKey={'teamName'}
-          />
-          <div className="flex flex-wrap items-center justify-start lg:justify-center gap-2 overflow-x-auto scroll-smooth">
-            {Object.entries(chartConfig).map(([dataKey, value]) => (
-              // biome-ignore lint/a11y/useKeyWithClickEvents: <explanation>
-              <div
-                key={dataKey}
-                className="text-white px-2 py-1 rounded max-sm:text-xs text-sm cursor-pointer whitespace-nowrap"
-                style={{
-                  background: value.isActive ? value.color : 'gray',
-                  whiteSpace: 'nowrap',
-                }}
-                onClick={() => handleConfig(dataKey)}
-              >
-                {value.label}
-              </div>
-            ))}
-          </div>
-        </div>
-      )}
-    </div>
+    <TeamRankingView
+      tableData={mockRanking}
+      chartData={mockBatterData.data.list}
+      columns={teamBatterRankColumns}
+      chartConfig={TeamRankingBatterConfig}
+      domain="all"
+    />
   );
 }
 
--- src/features/game/components/ranking/team/TeamPitcherRankingView.tsx
+++ src/features/game/components/ranking/team/TeamPitcherRankingView.tsx
@@ -1,23 +1,12 @@
 // import { TeamPitcherRank } from '@/features/common/types/pitchers';
 // import { useTeamRank } from "@/features/game/hooks/ranking/useTeamRank";
 import { teamPitcher as mockRanking } from '@/assets/data/__test__/mockRanking.json';
-import data from '@/assets/data/__test__/season-team-rank/seasonPitcher.json';
+import mockPitcherData from '@/assets/data/__test__/season-team-rank/seasonPitcher.json';
 import { TeamRankingPitcherConfig } from '@/constants/chart-config';
 import { teamPitcherRankColumns } from '@/constants/columns/team-rank-colums';
-import CustomBarChart from '@/features/common/CustomBarChart';
-import DataTable from '@/features/common/DataTable';
-import { Config } from '@/features/player/components/PlayerRecordChart';
-import { SeasonPitcher } from '@/features/player/types/record';
-import { useState } from 'react';
-
-const seasonPitcherData: SeasonPitcher[] = data.data.list;
+import TeamRankingView from '../common/TeamRankingView';
 
 function TeamPitcherRankingView() {
-  const [selectedTab, setSelectedTab] = useState<'table' | 'chart'>('table');
-  const [chartConfig, setChartConfig] = useState<Config>(
-    TeamRankingPitcherConfig
-  );
-
   // const { ranking, loading, error } = useTeamRank('pitcher');
 
   // if (!ranking.length || loading) {
@@ -28,84 +17,14 @@ function TeamPitcherRankingView() {
   //   return <div>{error}</div>;
   // }
 
-  const handleConfig = (dataKey: keyof Config) => {
-    const currentConfig = Object.keys(chartConfig).filter(
-      (key) => chartConfig[key].isActive
-    )[0];
-
-    if (currentConfig === dataKey) {
-      return;
-    }
-
-    setChartConfig((prev) => {
-      // 모든 항목을 비활성화한 뒤 클릭한 버튼만 활성화
-      const newConfig = Object.keys(prev).reduce((acc, key) => {
-        acc[key] = { ...prev[key], isActive: false }; // 모든 항목 비활성화
-        return acc;
-      }, {} as Config);
-
-      // 클릭한 버튼만 활성화
-      newConfig[dataKey] = {
-        ...prev[dataKey],
-        isActive: !prev[dataKey].isActive,
-      };
-      return newConfig;
-    });
-  };
-
   return (
-    <div>
-      <div className="flex items-center gap-2 justify-end">
-        <button
-          type="button"
-          className={`text-center w-12 bg-white text-black px-2 py-1 text-sm rounded ${
-            selectedTab === 'table' ? 'bg-wiz-red text-wiz-white' : 'bg-white'
-          }`}
-          onClick={() => setSelectedTab('table')}
-        >
-          표
-        </button>
-        <button
-          type="button"
-          className={`text-center w-12 bg-white text-black px-2 py-1 text-sm rounded ${
-            selectedTab === 'chart' ? 'bg-wiz-red text-wiz-white' : 'bg-white'
-          }`}
-          onClick={() => setSelectedTab('chart')}
-        >
-          차트
-        </button>
-      </div>
-      {selectedTab === 'table' ? (
-        <DataTable
-          data={mockRanking}
-          columns={teamPitcherRankColumns}
-          domain="all"
-        />
-      ) : (
-        <div>
-          <CustomBarChart
-            data={seasonPitcherData}
-            config={chartConfig}
-            XAxisKey={'teamName'}
-          />
-          <div className="flex flex-wrap items-center justify-start lg:justify-center gap-2 overflow-x-auto scroll-smooth">
-            {Object.entries(chartConfig).map(([dataKey, value]) => (
-              // biome-ignore lint/a11y/useKeyWithClickEvents: <explanation>
-              <div
-                key={dataKey}
-                className="text-white px-2 py-1 rounded max-sm:text-xs text-sm cursor-pointer whitespace-nowrap"
-                style={{
-                  background: value.isActive ? value.color : 'gray',
-                }}
-                onClick={() => handleConfig(dataKey)}
-              >
-                {value.label}
-              </div>
-            ))}
-          </div>
-        </div>
-      )}
-    </div>
+    <TeamRankingView
+      tableData={mockRanking}
+      chartData={mockPitcherData.data.list}
+      columns={teamPitcherRankColumns}
+      chartConfig={TeamRankingPitcherConfig}
+      domain="all"
+    />
   );
 }
 
