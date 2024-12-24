8f0cfcd541425ebcb20d6b42ee3a1c22f65ddb61
닮 <dhdpekfa1@daum.net>
2024-12-24 15:02:47 +0900

    feat: customBarChart 글자 크기 반응형 설정(#90)

--- src/features/common/CustomBarChart.tsx
+++ src/features/common/CustomBarChart.tsx
@@ -1,4 +1,5 @@
 import { ChartContainer } from '@/components/ui/chart';
+import { useEffect, useState } from 'react';
 import { Bar, BarChart, CartesianGrid, XAxis, YAxis } from 'recharts';
 import {
   RecentRecord,
@@ -20,15 +21,43 @@ interface CustomBarChartProps {
 }
 
 function CustomBarChart({ data, config, XAxisKey }: CustomBarChartProps) {
+  const [fontSize, setFontSize] = useState('16px');
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 768) {
+        // 모바일 화면 크기 기준
+        setFontSize('10px');
+      } else {
+        setFontSize('16px');
+      }
+    };
+
+    // 초기 화면 크기 설정
+    handleResize();
+    // 화면 크기 변경 감지
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <ChartContainer config={config} className="w-full h-52 mt-4">
       <BarChart accessibilityLayer data={data}>
         <CartesianGrid vertical={false} strokeOpacity={0.1} />
-        <XAxis dataKey={XAxisKey} tickLine={false} axisLine={false} />
+        <XAxis
+          dataKey={XAxisKey}
+          tickLine={false}
+          axisLine={false}
+          tick={{ fontSize }}
+        />
         <YAxis
           tickLine={false}
           axisLine={false}
           domain={['auto', (dataMax: number) => dataMax * 1.1]}
+          tick={{ fontSize }}
         />
         {Object.entries(config).map(([dataKey, value]) =>
           value.isActive ? (
