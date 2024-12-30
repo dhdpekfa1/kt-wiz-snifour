0318b2ca4739ef057924d91449db48a9d56fa8f4
닮 <dhdpekfa1@daum.net>
2024-12-30 17:34:41 +0900

    feat: 로그인 페이지 퍼블리싱(#122)

---
+++ src/pages/auth/LoginPage.tsx
@@ -0,0 +1,137 @@
+import {
+  Button,
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+  Checkbox,
+  Input,
+  Label,
+} from '@/components/ui';
+import Banner from '@/features/common/Banner';
+import Breadcrumb from '@/features/common/Breadcrumb';
+import Layout from '@/features/common/Layout';
+import { Eye, EyeOff } from 'lucide-react';
+import { useState } from 'react';
+
+const LoginPage = () => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [saveId, setSaveId] = useState<boolean>(false);
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  const togglePassword = () => setShowPassword((prevState) => !prevState);
+
+  const handleMoveRegistration = () => {
+    console.log('TODO: 회원가입 페이지로 이동');
+  };
+
+  const handleLogin = () => {
+    console.log('로그인 시도', { email, password, saveId });
+    // `saveId` 상태에 따라 추가 로직 구현 가능
+  };
+
+  return (
+    <Layout
+      header={
+        <Banner>
+          <Banner.Image
+            src="https://placehold.co/1200x200/141414/642521?text=KT+Family"
+            alt="login"
+          />
+          <Banner.Overlay>
+            <Banner.Heading title="Login" subtitle="kt wiz의 가족" />
+            <Banner.Description description="로그인 후 kt wiz 사이트를 더욱 다양하게 이용해 보세요. 더욱 다양하게 이용해 보세요." />
+          </Banner.Overlay>
+        </Banner>
+      }
+    >
+      <Breadcrumb />
+      <div className="flex items-center flex-col my-10 md:my-20">
+        <Card className="w-full md:w-1/2 lg:1/3">
+          <CardHeader>
+            <CardTitle className="text-xl md:text-2xl text-center">
+              Login
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="flex flex-col gap-4">
+              <div className="flex flex-col gap-2">
+                <Label className="text-sm md:text-lg" htmlFor="email">
+                  이메일
+                </Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="이메일을 입력하세요."
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                  className="text-xs md:text-sm"
+                />
+              </div>
+              <div className="relative flex flex-col gap-2">
+                <Label className="text-sm md:text-lg" htmlFor="password">
+                  비밀번호
+                </Label>
+                <Input
+                  id="password"
+                  type={showPassword ? 'text' : 'password'}
+                  placeholder="비밀번호를 입력하세요."
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                  className="text-xs md:text-sm"
+                />
+                <Button
+                  size="icon"
+                  className="absolute top-1/2 right-2 -translate-y-1/5 bg-transparent hover:bg-transparent"
+                  onClick={togglePassword}
+                >
+                  {showPassword ? (
+                    <Eye className="h-5 w-5 text-muted-foreground" />
+                  ) : (
+                    <EyeOff className="h-5 w-5 text-muted-foreground" />
+                  )}
+                </Button>
+              </div>
+              <div className="flex items-center gap-2">
+                <Checkbox
+                  id="saveId"
+                  checked={saveId}
+                  onCheckedChange={(checked) => setSaveId(!!checked)}
+                />
+                <Label
+                  htmlFor="saveId"
+                  className="text-xs md:text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                >
+                  아이디 저장
+                </Label>
+              </div>
+            </div>
+          </CardContent>
+          <CardFooter className="flex flex-col mt-6 gap-2">
+            <Button
+              className="w-full m text-white bg-wiz-red bg-opacity-50 hover:bg-wiz-red hover:bg-opacity-70"
+              onClick={handleLogin}
+            >
+              로그인
+            </Button>
+            <div className="mt-4 text-center text-sm">
+              계정이 없으신가요?
+              <Button
+                onClick={handleMoveRegistration}
+                className="underline text-sm ml-1"
+              >
+                회원가입
+              </Button>
+            </div>
+          </CardFooter>
+        </Card>
+      </div>
+    </Layout>
+  );
+};
+
+export default LoginPage;
