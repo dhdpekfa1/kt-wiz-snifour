fec762f2394f6f37a3775cb959d34c7497b16664
Miso Kim <kimms9725@naver.com>
2024-12-30 14:20:31 +0900

    feat: vs table 반응형(#114)

--- src/features/game/components/ranking/team/TeamVSTable.tsx
+++ src/features/game/components/ranking/team/TeamVSTable.tsx
@@ -34,67 +34,75 @@ function TeamVSTable() {
   }
 
   return (
-    <Table className="mt-4">
-      <TableHeader>
-        <TableRow className="font-semibold border-none">
-          <TableHead className="text-center bg-wiz-white bg-opacity-30">
-            팀명
-          </TableHead>
-          {vsOrder.map(({ teamCode, teamName }) => (
-            <TableHead
-              key={`th-${teamCode}`}
+    <div>
+      <p
+        className={cn('text-neutral-400 text-xs', 'md:text-sm', 'lg:text-base')}
+      >
+        팀 간 승-패-무 정보입니다.
+      </p>
+      <Table className="mt-4">
+        <TableHeader>
+          <TableRow className="font-semibold border-none">
+            <TableHead className="text-center bg-wiz-white bg-opacity-30">
+              팀명
+            </TableHead>
+            {vsOrder.map(({ teamCode, teamName }) => (
+              <TableHead
+                key={`th-${teamCode}`}
+                className={cn(
+                  'text-center bg-wiz-white bg-opacity-30 text-xs',
+                  'md:text-sm',
+                  'lg:text-base',
+                  teamCode === 'KT' && 'bg-wiz-red bg-opacity-70'
+                )}
+              >
+                {teamName}
+              </TableHead>
+            ))}
+          </TableRow>
+        </TableHeader>
+        <TableBody className="text-center">
+          {vsOrder.map(({ teamCode, teamName }, index) => (
+            <TableRow
+              key={`tr-${teamCode}`}
               className={cn(
-                'text-center bg-wiz-white bg-opacity-30',
-                teamCode === 'KT' && 'bg-wiz-red bg-opacity-70'
+                'border-b-wiz-white border-opacity-10',
+                teamCode === 'KT' && 'bg-wiz-red bg-opacity-70 font-bold'
               )}
             >
-              {teamName}
-              <br />
-              (승-패-무)
-            </TableHead>
-          ))}
-        </TableRow>
-      </TableHeader>
-      <TableBody className="text-center">
-        {vsOrder.map(({ teamCode, teamName }, index) => (
-          <TableRow
-            key={`tr-${teamCode}`}
-            className={cn(
-              'border-b-wiz-white border-opacity-10',
-              teamCode === 'KT' && 'bg-wiz-red bg-opacity-70 font-bold'
-            )}
-          >
-            <TableCell>{teamName}</TableCell>
-            {vsOrder.map((vsTeam, vIndex) => {
-              const { win, lose, drawn }: TeamVSResult = ((
-                vs as ArrangedTeamVS[]
-              )[index][vsTeam.teamCode] as TeamVSResult) || {
-                win: '',
-                lose: '',
-                drawn: '',
-              };
+              <TableCell>{teamName}</TableCell>
+              {vsOrder.map((vsTeam, vIndex) => {
+                const { win, lose, drawn }: TeamVSResult = ((
+                  vs as ArrangedTeamVS[]
+                )[index][vsTeam.teamCode] as TeamVSResult) || {
+                  win: '',
+                  lose: '',
+                  drawn: '',
+                };
 
-              return (
-                <TableCell
-                  key={`${teamCode}-${vsTeam.teamCode}`}
-                  className={cn(
-                    index > 0 &&
-                      vIndex === 0 &&
-                      'bg-wiz-red bg-opacity-70 font-bold' // 중복 색칠 방지(배경색 투명도가 있기 때문)
-                  )}
-                >
-                  {teamCode !== vsTeam.teamCode && (
-                    <span>
-                      {win}-{lose}-{drawn}
-                    </span>
-                  )}
-                </TableCell>
-              );
-            })}
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+                return (
+                  <TableCell
+                    key={`${teamCode}-${vsTeam.teamCode}`}
+                    className={cn(
+                      'w-fit',
+                      index > 0 &&
+                        vIndex === 0 &&
+                        'bg-wiz-red bg-opacity-70 font-bold' // 중복 색칠 방지(배경색 투명도가 있기 때문)
+                    )}
+                  >
+                    {teamCode !== vsTeam.teamCode && (
+                      <p>
+                        {win}-{lose}-{drawn}
+                      </p>
+                    )}
+                  </TableCell>
+                );
+              })}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
   );
 }
 
