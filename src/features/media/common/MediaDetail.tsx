67d19eecc76f708ab86af76f282e80f5c267d63c
Miso Kim <kimms9725@naver.com>
2025-01-01 19:11:31 +0900

    style: width 조절(#128)

--- src/features/media/common/MediaDetail.tsx
+++ src/features/media/common/MediaDetail.tsx
@@ -38,7 +38,7 @@ const MediaDetailContainer = ({
   className?: string;
 }) => (
   <div
-    className={`max-w-4xl  mx-auto min-h-[1100px] px-4 sm:px-6 lg:px-8 py-8 ${className}`}
+    className={`w-full mx-auto min-h-[1100px] px-4 sm:px-6 lg:px-8 py-8 ${className}`}
   >
     {children || null}
   </div>
