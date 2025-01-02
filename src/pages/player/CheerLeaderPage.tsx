215b97263518d17086a633d9d98c8f2120039b6d
Mina Lee <mal0070@naver.com>
2025-01-02 16:40:10 +0900

    feat: 상단 배너 추가(#126)

--- src/pages/player/CheerLeaderPage.tsx
+++ src/pages/player/CheerLeaderPage.tsx
@@ -1,4 +1,6 @@
+import Banner from '@/features/common/Banner';
 import Breadcrumb from '@/features/common/Breadcrumb';
+import Layout from '@/features/common/Layout';
 import CheerleaderDialog from '@/features/player/components/cheerleader/CheerleaderDialog';
 import useCheerleaderList from '@/features/player/hooks/useCheerleaderList';
 
@@ -10,16 +12,27 @@ function CheerleaderPage() {
   if (error) return <div>{error}</div>;
 
   return (
-    <div className="w-[1200px]">
-      {/* breadcrumbs */}
+    <Layout
+      header={
+        <Banner>
+          <Banner.Image
+            src="https://placehold.co/1200x200/141414/642521?text=CHEERLEADER"
+            alt="KT WIZ Coach"
+          />
+          <Banner.Overlay>
+            <Banner.Heading title="KT WIZ 선수단" subtitle="응원단" />
+            <Banner.Description description="KT wiz의 꽃! KT wiz의 응원단" />
+          </Banner.Overlay>
+        </Banner>
+      }
+    >
       <Breadcrumb />
-      {/* 응원단 컴포넌트 :  TODO - 1.고유 넘버로 key 수정  2.Data가 로딩 중 일 때 스켈레톤 추가*/}
       <div className="text-wiz-white grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
         {cheerleaderList.map((cheerleader) => (
           <CheerleaderDialog data={cheerleader} key={cheerleader.leaderName} />
         ))}
       </div>
-    </div>
+    </Layout>
   );
 }
 
