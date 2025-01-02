01a54b78774cad8c0483da5df8b157cb843d7fe8
닮 <dhdpekfa1@daum.net>
2025-01-02 17:41:44 +0900

    feat: 홈 wizVideo 애니메이션 적용(#129)

---
+++ src/features/home/components/WizVideoAnimation.tsx
@@ -0,0 +1,70 @@
+import { cn } from '@/lib/utils';
+import React from 'react';
+import { useInView } from 'react-intersection-observer';
+import { useNavigate } from 'react-router';
+import { Video } from '../types';
+
+interface WizVideoAnimationProps {
+  vid: Video;
+  index: number;
+  navigate: ReturnType<typeof useNavigate>;
+}
+
+const WizVideoAnimation: React.FC<WizVideoAnimationProps> = ({
+  vid,
+  index,
+  navigate,
+}) => {
+  const { ref, inView } = useInView({
+    threshold: 0.1, // 10% 화면에 들어오면 트리거
+    // triggerOnce: true, // 한 번만 실행
+  });
+
+  return (
+    <div
+      ref={ref}
+      className={cn(
+        'flex flex-col gap-2 bg-white bg-opacity-10 rounded-xl overflow-hidden cursor-pointer text-white transition-all duration-500 ease-in-out transform',
+        inView ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'
+      )}
+      style={{
+        transitionDelay: `${index * 200}ms`, // 각 항목의 지연 시간
+      }}
+      role="button"
+      tabIndex={0}
+      onClick={() => {
+        if (vid.artcSeq) {
+          navigate(`/media/highlight/${vid.artcSeq}`);
+        }
+      }}
+      onKeyDown={(e) => {
+        if (e.key === 'Enter' || e.key === ' ') {
+          if (vid.artcSeq) {
+            navigate(`/media/highlight/${vid.artcSeq}`);
+          }
+        }
+      }}
+    >
+      <div className="w-full h-fit bg-gray-500">
+        <img src={vid.imgFilePath} alt={vid.artcTitle} />
+      </div>
+      <div className="h-full flex flex-col justify-between px-2 py-1 gap-4 md:gap-8">
+        <p
+          className={cn('font-semibold text-xs', 'md:text-sm', 'lg:text-base')}
+        >
+          {vid.artcTitle}
+        </p>
+        <p
+          className={cn(
+            'text-neutral-400 self-end text-[0.6rem]',
+            'lg:text-xs'
+          )}
+        >
+          {vid.contentsDate}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export { WizVideoAnimation };
--- src/features/home/index.ts
+++ src/features/home/index.ts
@@ -3,4 +3,5 @@ export { MainImageSlider } from './components/MainImageSlider';
 export { TeamRanking } from './components/TeamRanking';
 export { MatchInfo } from './components/MatchInfo';
 export { WizVideo } from './components/WizVideo';
+export { WizVideoAnimation } from './components/WizVideoAnimation';
 export { WizGallery } from './components/WizGallery';
