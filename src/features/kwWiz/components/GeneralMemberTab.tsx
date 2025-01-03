2fcc24f8d8490967feff3503fc974c187a611747
닮 <dhdpekfa1@daum.net>
2025-01-03 17:52:47 +0900

    feat: 일반 회원 페이지 이미지 추가(#153)

--- src/features/kwWiz/components/GeneralMemberTab.tsx
+++ src/features/kwWiz/components/GeneralMemberTab.tsx
@@ -1,5 +1,16 @@
+import Breadcrumb from '@/features/common/Breadcrumb';
+
 const GeneralMemberTab = () => {
-  return <div className="text-white">GeneralMemberTab</div>;
+  return (
+    <div className="w-full h-full">
+      <Breadcrumb />
+      <img
+        className="w-full h-full object-cover"
+        src="/assets/Ktwiz/general-member.jpg"
+        alt="general-member"
+      />
+    </div>
+  );
 };
 
 export { GeneralMemberTab };
