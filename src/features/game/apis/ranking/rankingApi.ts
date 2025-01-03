3b7f8ccc0f976a0fead61569ed19c0ac9e3e5d82
Miso Kim <kimms9725@naver.com>
2025-01-04 01:01:22 +0900

    remove: 기존 axios 기반 투수, 타자 api 호출 파일 삭제(#150)

--- src/features/game/apis/ranking/rankingApi.ts
+++ src/features/game/apis/ranking/rankingApi.ts
@@ -1,40 +1,52 @@
 import instance from '@/lib/axios/instance';
-import { RankingDto } from '../../types/ranking';
+import {
+  BatterRankingResponse,
+  PitcherRankingResponse,
+  RankingDto,
+} from '../../types/ranking';
 import { ApiRoutes } from '@/constants/route';
 
 // ranking api
 export const rankingApi = {
   // 투수
-  getPitcherEraTop3: async () => {
+  getPitcherEraTop3: async (): Promise<PitcherRankingResponse> => {
     const response = await instance.get(ApiRoutes.PitcherEraTop3);
     return response.data;
   },
-  getPitcherWinTop3: async () => {
+  getPitcherWinTop3: async (): Promise<PitcherRankingResponse> => {
     const response = await instance.get(ApiRoutes.PitcherWinTop3);
     return response.data;
   },
-  getKTPitcherRanking: async (params?: RankingDto) => {
+  getKTPitcherRanking: async (
+    params?: RankingDto
+  ): Promise<PitcherRankingResponse> => {
     const response = await instance.get(ApiRoutes.KtPitcherRank, { params });
     return response.data;
   },
-  getAllPitcherRanking: async (params?: RankingDto) => {
+  getAllPitcherRanking: async (
+    params?: RankingDto
+  ): Promise<PitcherRankingResponse> => {
     const response = await instance.get(ApiRoutes.AllPitcherRank, { params });
     return response.data;
   },
   // 타자
-  getBatterHraTop3: async () => {
+  getBatterHraTop3: async (): Promise<BatterRankingResponse> => {
     const response = await instance.get(ApiRoutes.BatterHraTop3);
     return response.data;
   },
-  getBatterHrTop3: async () => {
+  getBatterHrTop3: async (): Promise<BatterRankingResponse> => {
     const response = await instance.get(ApiRoutes.BatterHrTop3);
     return response.data;
   },
-  getKTBatterRanking: async (params?: RankingDto) => {
+  getKTBatterRanking: async (
+    params?: RankingDto
+  ): Promise<BatterRankingResponse> => {
     const response = await instance.get(ApiRoutes.KTBatterRank, { params });
     return response.data;
   },
-  getAllBatterRanking: async (params?: RankingDto) => {
+  getAllBatterRanking: async (
+    params?: RankingDto
+  ): Promise<BatterRankingResponse> => {
     const response = await instance.get(ApiRoutes.AllBatterRank, { params });
     return response.data;
   },
