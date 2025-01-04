ca266f6a2a64d7d81c9cd7e3a6c96750d67e671c
닮 <dhdpekfa1@daum.net>
2025-01-04 10:40:50 +0900

    feat: 기부 프로그램 이미지 추가(#153)

--- src/features/kwWiz/components/DonationProgramTab.tsx
+++ src/features/kwWiz/components/DonationProgramTab.tsx
@@ -1,5 +1,15 @@
+import Breadcrumb from '@/features/common/Breadcrumb';
 const DonationProgramTab = () => {
-  return <div className="text-white">DonationProgramTab</div>;
+  return (
+    <div className="w-full h-full">
+      <Breadcrumb />
+      <img
+        className="w-full h-full object-cover"
+        src="/assets/Ktwiz/donation-program.jpg"
+        alt="donation-program"
+      />
+    </div>
+  );
 };
 
 export { DonationProgramTab };
