4aa85e8cdb8bad2e63b0dc676a9a0f875c0eadcf
닮 <dhdpekfa1@daum.net>
2025-01-05 15:24:57 +0900

    feat: 경기 일정 호출 함수 리액트쿼리 적용(#157)

--- src/features/game/components/calender/MatchCalendar.tsx
+++ src/features/game/components/calender/MatchCalendar.tsx
@@ -1,47 +1,49 @@
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui";
 import {
   CalenderBody,
   MatchCalendarCell,
-} from '@/features/game//components/calender';
-import { getAllMonthSchedule, getMonthSchedule } from '@/features/game/apis';
-import { GameSchedule } from '@/features/game/types/match-schedule';
-import { useMatchStore } from '@/store/useMatchStore';
-import { format } from 'date-fns';
-import { useEffect, useState } from 'react';
+} from "@/features/game/components/calender";
+import { useMatchStore } from "@/store/useMatchStore";
+import { format } from "date-fns";
+import { useState } from "react";
+import { useGetMatchScheduleQuery } from "@/features/game/apis/match-schedule/matchScheduleApi.query";
 
 const GAME_TABS_CONFIG = [
-  { value: 'ktWiz', label: 'KT Wiz 경기' },
-  { value: 'allLeague', label: '전체 리그' },
+  { value: "ktWiz", label: "KT Wiz 경기" },
+  { value: "allLeague", label: "전체 리그" },
 ];
 
 const MatchCalendar = () => {
-  const [ktMatchData, setKTMatchData] = useState<GameSchedule[]>();
-  const [allMatchData, setAllMatchData] = useState<GameSchedule[]>();
   const { currentMonth } = useMatchStore();
-  const [currentTab, setCurrentTab] = useState<'ktWiz' | 'allLeague'>('ktWiz');
+  const [currentTab, setCurrentTab] = useState<"ktWiz" | "allLeague">("ktWiz");
 
-  useEffect(() => {
-    const fetchMatchSchedule = async () => {
-      const yearMonth = format(currentMonth, 'yyyyMM');
-      if (currentTab === 'ktWiz') {
-        const data = await getMonthSchedule(yearMonth);
-        setKTMatchData(data);
-      } else if (currentTab === 'allLeague') {
-        const data = await getAllMonthSchedule(yearMonth);
-        setAllMatchData(data);
-      }
-    };
-    fetchMatchSchedule();
-  }, [currentMonth, currentTab]);
+  const { matchData: ktMatchData = [], isLoading: isLoadingKT } =
+    useGetMatchScheduleQuery({
+      currentMonth,
+      type: "kt",
+    });
 
+  const { matchData: allMatchData = [], isLoading: isLoadingAll } =
+    useGetMatchScheduleQuery({
+      currentMonth,
+      type: "all",
+    });
+
+  // 날짜별 데이터 렌더링
   const renderCellContent = (date: Date) => {
-    const formattedDate = format(date, 'yyyyMMdd');
-    const match = ktMatchData?.find(
-      (item) => item.gameDate.toString() === formattedDate
-    );
-    const matches = allMatchData?.filter(
-      (item) => item.gameDate.toString() === formattedDate
-    );
+    const formattedDate = format(date, "yyyyMMdd");
+
+    const match =
+      currentTab === "ktWiz"
+        ? ktMatchData.find((item) => item.gameDate.toString() === formattedDate)
+        : undefined;
+
+    const matches =
+      currentTab === "allLeague"
+        ? allMatchData.filter(
+            (item) => item.gameDate.toString() === formattedDate
+          )
+        : undefined;
 
     return (
       <MatchCalendarCell
@@ -53,8 +55,9 @@ const MatchCalendar = () => {
     );
   };
 
+  // 탭 변경 핸들러
   const handleTabChange = (value: string) => {
-    setCurrentTab(value as 'ktWiz' | 'allLeague');
+    setCurrentTab(value as "ktWiz" | "allLeague");
   };
 
   return (
@@ -71,9 +74,6 @@ const MatchCalendar = () => {
               <TabsTrigger
                 key={tab.value}
                 value={tab.value}
-                onClick={() =>
-                  handleTabChange(tab.value as 'ktWiz' | 'allLeague')
-                }
                 className="media-tabs-trigger px-6 py-2.5"
               >
                 {tab.label}
@@ -82,16 +82,24 @@ const MatchCalendar = () => {
           </TabsList>
           <div className="flex gap-2 text-xs md:text-sm lg:text-base text-white">
             <span className="px-2 lg:px-3 py-2 rounded bg-red-500">승</span>
-            <span className="px-2 lg:px-3 py-2  rounded bg-gray-700">패</span>
-            <span className="px-2 lg:px-3 py-2  rounded bg-gray-500">무</span>
+            <span className="px-2 lg:px-3 py-2 rounded bg-gray-700">패</span>
+            <span className="px-2 lg:px-3 py-2 rounded bg-gray-500">무</span>
           </div>
         </div>
         {/* 탭 컨텐츠 */}
         <TabsContent value="ktWiz">
-          <CalenderBody renderCellContent={renderCellContent} />
+          {isLoadingKT ? (
+            <p>Loading...</p>
+          ) : (
+            <CalenderBody renderCellContent={renderCellContent} />
+          )}
         </TabsContent>
         <TabsContent value="allLeague">
-          <CalenderBody renderCellContent={renderCellContent} />
+          {isLoadingAll ? (
+            <p>Loading...</p>
+          ) : (
+            <CalenderBody renderCellContent={renderCellContent} />
+          )}
         </TabsContent>
       </Tabs>
     </div>
--- src/features/game/components/carousel/MatchInfoCarousel.tsx
+++ src/features/game/components/carousel/MatchInfoCarousel.tsx
@@ -4,25 +4,29 @@ import {
   CarouselContent,
   CarouselNext,
   CarouselPrevious,
-} from '@/components/ui';
-import { CarouselCard } from '@/features/game/components/carousel';
-import { useMatchSchedule } from '@/hooks/useMatchSchedule';
-import { useMatchStore } from '@/store/useMatchStore';
-import { format, isValid, parse } from 'date-fns';
-import { useEffect, useState } from 'react';
+} from "@/components/ui";
+import { CarouselCard } from "@/features/game/components/carousel";
+import { useGetMatchScheduleQuery } from "@/features/game/apis/match-schedule/matchScheduleApi.query";
+import { useMatchStore } from "@/store/useMatchStore";
+import { format, isValid, parse } from "date-fns";
+import { useEffect, useState } from "react";
 
 const MatchInfoCarousel = () => {
   const [carouselApi, setCarouselApi] = useState<CarouselApi | null>(null);
   const { currentMonth, selectedDate } = useMatchStore();
-  const { matchData } = useMatchSchedule({
+  const { matchData = [], isLoading } = useGetMatchScheduleQuery({
     currentMonth,
     carouselApi,
   });
 
+  if (isLoading) {
+    // TODO: 스켈레톤 작업
+  }
+
   /**  날짜 선택 시 캐러셀 이동 */
   useEffect(() => {
     if (!selectedDate || !carouselApi || matchData.length === 0) return;
-    const formattedSelectedDate = format(selectedDate, 'yyyyMMdd'); // 형식 변환 변환
+    const formattedSelectedDate = format(selectedDate, "yyyyMMdd"); // 형식 변환 변환
 
     // matchData에서 formattedSelectedDate와 일치하는 인덱스 찾기
     const selectedIndex = matchData.findIndex(
@@ -38,7 +42,7 @@ const MatchInfoCarousel = () => {
   const hasUpcomingGames = matchData.some((game) => {
     const gameDateString = game.gameDate?.toString();
     if (!gameDateString) return false;
-    const parsedDate = parse(gameDateString, 'yyyyMMdd', new Date());
+    const parsedDate = parse(gameDateString, "yyyyMMdd", new Date());
     return isValid(parsedDate) && parsedDate >= today; // 유효한 날짜인지 확인 후 비교
   });
 
