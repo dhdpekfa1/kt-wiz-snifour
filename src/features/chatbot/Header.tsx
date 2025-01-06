16af56bef9b70c049a9d44a7ba409df9356d1cca
Mina Lee <mal0070@naver.com>
2025-01-06 17:46:53 +0900

    feat: 버튼을 이용하여 챗봇을 열고 닫는 기능 구현(#139)

--- src/features/chatbot/Header.tsx
+++ src/features/chatbot/Header.tsx
@@ -2,13 +2,14 @@ import { X } from 'lucide-react';
 
 const Header = () => {
   const handleClose = () => {
-    return <div>초기 UI</div>;
+    // 커스텀 이벤트 발생
+    const event = new CustomEvent('closeChatbot');
+    window.dispatchEvent(event);
   };
-
   return (
     <div className="w-full flex justify-between items-center p-5 bg-wiz-red text-wiz-white">
       <h4 className="font-semibold">KT wiz 챗봇</h4>
-      <X onClick={handleClose} />
+      <X onClick={handleClose} className="cursor-pointer" />
     </div>
   );
 };
