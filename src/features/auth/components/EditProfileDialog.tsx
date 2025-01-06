87e1ee067ff30ae44535f53d08d199f7878ef090
닮 <dhdpekfa1@daum.net>
2025-01-06 17:21:46 +0900

    feat: 작은 화면 헤더 nav 추가(#161)

--- src/features/auth/components/EditProfileDialog.tsx
+++ src/features/auth/components/EditProfileDialog.tsx
@@ -1,8 +1,7 @@
-import { ReactNode, useState } from 'react';
+import { ReactNode } from 'react';
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
@@ -15,27 +14,40 @@ import { useUserStore } from '@/store/useUserStore';
 import { supabase } from '@/lib/supabase';
 import { useNavigate } from 'react-router';
 import useUserSession from '../hooks/useUserSession';
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { nicknameSchema } from '../schemas/signupSchema';
+
+export type NicknameValue = z.infer<typeof nicknameSchema>;
 
 const EditProfileDialog = ({ children }: { children: ReactNode }) => {
   const { setNickname, resetUser } = useUserStore();
   const { email, nickname, sub } = useUserSession();
-  const [localNickname, setLocalNickname] = useState<string | null>(nickname);
-  const [isOpen, setIsOpen] = useState(false);
   const navigate = useNavigate();
 
-  const handleSave = async () => {
-    if (sub && localNickname !== nickname) {
-      const { data, error } = await supabase.auth.updateUser({
-        data: { nickname: localNickname },
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<NicknameValue>({
+    resolver: zodResolver(nicknameSchema),
+    defaultValues: {
+      nickname: nickname || '',
+    },
+  });
+
+  const handleSave = async (data: NicknameValue) => {
+    if (sub && data.nickname !== nickname) {
+      const { error } = await supabase.auth.updateUser({
+        data: { nickname: data.nickname },
       });
       if (error) {
         alert('닉네임 변경에 실패했습니다.\n다시 시도해주세요.');
+        return;
       }
-      if (data && localNickname && localNickname !== nickname) {
-        setNickname(localNickname);
-        setIsOpen(false);
-        alert('닉네임이 변경되었습니다.');
-      }
+      setNickname(data.nickname);
+      alert('닉네임이 변경되었습니다.');
     }
   };
 
@@ -43,65 +55,78 @@ const EditProfileDialog = ({ children }: { children: ReactNode }) => {
     const { error } = await supabase.auth.signOut();
     if (error) {
       alert('로그아웃에 실패했습니다.\n다시 시도해주세요.');
+      return;
     }
-    setIsOpen(false);
     resetUser();
     alert('로그아웃되었습니다.');
     navigate('/'); // 홈으로 이동
   };
 
   return (
-    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+    <Dialog>
       <DialogTrigger asChild>
-        <Button variant="outline">{children}</Button>
+        <Button
+          variant={null}
+          className="-ml-3 lg:border lg:border-wiz-white/20"
+        >
+          {children}
+        </Button>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px] bg-wiz-black text-wiz-white border-wiz-red border-opacity-30">
+      <DialogContent className="max-w-[360px] bg-wiz-black text-wiz-white border-wiz-red border-opacity-30 rounded-md">
         <DialogHeader>
-          <DialogTitle>회원 정보 수정</DialogTitle>
-          <DialogDescription>닉네임을 변경할 수 있습니다.</DialogDescription>
+          <DialogTitle className="text-base md:text-lg">회원 정보</DialogTitle>
         </DialogHeader>
-        <div className="grid gap-4 py-4">
+        <form
+          onSubmit={handleSubmit(handleSave)}
+          className="flex flex-col gap-4 py-4"
+        >
           {/* 이메일 (비활성화) */}
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="email" className="text-right">
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="email" className="text-sm md:text-base">
               이메일
             </Label>
             <Input
               disabled
               id="email"
               value={email || '이메일 없음'}
-              className="col-span-3"
+              className="text-xs md:text-sm bg-wiz-black text-wiz-white"
             />
           </div>
           {/* 닉네임 (수정 가능) */}
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="nickname" className="text-right">
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="nickname" className="text-sm md:text-base">
               닉네임
             </Label>
             <Input
               id="nickname"
-              value={localNickname || ''}
-              onChange={(e) => setLocalNickname(e.target.value)}
-              className="col-span-3"
+              type="text"
+              autoComplete="off"
+              placeholder="닉네임을 입력하세요."
+              {...register('nickname')}
+              className="text-xs md:text-sm bg-wiz-black text-wiz-white"
             />
+            {errors.nickname && (
+              <p className="text-red-500 text-xs mt-1">
+                * {errors.nickname.message}
+              </p>
+            )}
           </div>
-        </div>
-        <DialogFooter>
-          <Button
-            type="button"
-            onClick={handleLogout}
-            className="bg-wiz-red bg-opacity-20 hover:bg-wiz-red hover:bg-opacity-30"
-          >
-            로그아웃
-          </Button>
-          <Button
-            type="button"
-            onClick={handleSave}
-            className="bg-wiz-white bg-opacity-10 hover:bg-wiz-white hover:bg-opacity-20"
-          >
-            저장
-          </Button>
-        </DialogFooter>
+          <DialogFooter className="flex gap-2">
+            <Button
+              type="button"
+              onClick={handleLogout}
+              className="bg-wiz-red bg-opacity-20 hover:bg-wiz-red hover:bg-opacity-30 w-full"
+            >
+              로그아웃
+            </Button>
+            <Button
+              type="submit"
+              className="bg-wiz-white bg-opacity-10 hover:bg-wiz-white hover:bg-opacity-20 w-full"
+            >
+              저장
+            </Button>
+          </DialogFooter>
+        </form>
       </DialogContent>
     </Dialog>
   );
