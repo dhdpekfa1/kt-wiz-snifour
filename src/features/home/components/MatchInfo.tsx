a5c52180f4d201cd06e61311383e091db15bb773
Miso Kim <kimms9725@naver.com>
2025-01-08 13:10:59 +0900

    feat: 경기 정보 더보기 버튼(#165)

--- src/features/home/components/MatchInfo.tsx
+++ src/features/home/components/MatchInfo.tsx
@@ -7,6 +7,7 @@ import { useGetMatchScheduleQuery } from '@/features/game/apis/match-schedule/ma
 import MatchInfoCard from './MatchInfoCard';
 import useGetRecentMatchScheduleQuery from '@/features/game/apis/match-schedule/RecentScheduleApi.query';
 import { useMatchStore } from '@/store/useMatchStore';
+import { Link } from 'react-router';
 
 function MatchInfo() {
   useGetRecentMatchScheduleQuery();
@@ -40,42 +41,55 @@ function MatchInfo() {
   const matchOfToday = matchData[matchIndex];
 
   return (
-    <div
-      className={cn(
-        'w-full h-fit flex flex-col items-start rounded-3xl overflow-hidden bg-white',
-        'md:h-96 md:flex-row',
-        'lg:h-[500px]'
-      )}
-    >
-      <MatchInfoCard>
-        <MatchInfoCard.Header
-          match={matchData}
-          matchIndex={matchIndex}
-          handlePrevDay={handlePrevDay}
-          handleNextDay={handleNextDay}
-          loading={isLoading}
-        />
-        <MatchInfoCard.Content
-          match={matchData}
-          matchIndex={matchIndex}
-          loading={isLoading}
-        />
-      </MatchInfoCard>
-      {/* 사이드 */}
+    <>
       <div
         className={cn(
-          'w-full h-40 flex',
-          'md:w-[25%] md:border-l md:flex-col md:h-full'
+          'w-full h-fit flex flex-col items-start rounded-3xl overflow-hidden bg-white',
+          'md:h-96 md:flex-row',
+          'lg:h-[500px]'
         )}
       >
-        <TeamRanking />
-        <RecentMatches
-          match={matchData}
-          matchOfToday={matchOfToday}
-          loading={isLoading}
-        />
+        <MatchInfoCard>
+          <MatchInfoCard.Header
+            match={matchData}
+            matchIndex={matchIndex}
+            handlePrevDay={handlePrevDay}
+            handleNextDay={handleNextDay}
+            loading={isLoading}
+          />
+          <MatchInfoCard.Content
+            match={matchData}
+            matchIndex={matchIndex}
+            loading={isLoading}
+          />
+        </MatchInfoCard>
+        {/* 사이드 */}
+        <div
+          className={cn(
+            'w-full h-40 flex',
+            'md:w-[25%] md:border-l md:flex-col md:h-full'
+          )}
+        >
+          <TeamRanking />
+          <RecentMatches
+            match={matchData}
+            matchOfToday={matchOfToday}
+            loading={isLoading}
+          />
+        </div>
       </div>
-    </div>
+      <div className="flex items-center justify-center my-4">
+        <Link
+          to="/game/regular/schedule"
+          className={cn(
+            'rounded bg-white bg-opacity-10 text-white hover:bg-opacity-100 hover:text-black text-xs px-2 py-1 transition-colors duration-300',
+            'lg:text-base lg:px-4 lg:py-2'
+          )}
+        >
+          더 많은 경기보기
+        </Link>
+      </div>
+    </>
   );
 }
 
