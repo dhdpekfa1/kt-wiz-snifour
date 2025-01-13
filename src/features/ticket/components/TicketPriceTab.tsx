4083583bb26301d58ad4345bc2789a5cd2e5a6d9
닮 <dhdpekfa1@daum.net>
2025-01-13 16:14:23 +0900

    feat: 입장 요금 반응형(#200)

--- src/features/ticket/components/TicketPriceTab.tsx
+++ src/features/ticket/components/TicketPriceTab.tsx
@@ -3,21 +3,14 @@ import SubTitle from '@/features/common/SubTitle';
 
 const TicketPriceTab = () => {
   return (
-    <div className="bg-wiz-black text-wiz-white py-10 min-h-[600px]">
+    <div>
       <Breadcrumb />
-      <h1 className="text-wiz-white text-4xl text-center font-bold mt-8 mb-8">
-        입장 요금
-      </h1>
-      <div className="relative max-w-5xl mx-auto">
-        <SubTitle title="입장 요금" />
-        <div className="rounded-lg p-6 bg-opacity-10 bg-wiz-black mt-6 mb-6 flex justify-center">
-          <img
-            src="/assets/ticketpurchase/seat.png"
-            alt="좌석 정보"
-            className="w-full max-w-4xl rounded-lg shadow-lg"
-          />
-        </div>
-      </div>
+      <SubTitle title="입장 요금" className="mt-8 mb-4" />
+      <img
+        src="/assets/ticketpurchase/seat.png"
+        alt="좌석 정보"
+        className="w-full h-auto rounded-lg mb-8"
+      />
     </div>
   );
 };
